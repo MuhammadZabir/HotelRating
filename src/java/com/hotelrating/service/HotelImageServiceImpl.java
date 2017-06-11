2c9003c70b001fd7b77312988ded4761c91fd30f
Muhammad Zabir Abdul Halim <zabir.halim@mandrill.com.my>
2017-06-11 10:45:25 +0800

    Fix naming

--- src/java/com/hotelrating/service/HotelImageServiceImpl.java
+++ src/java/com/hotelrating/service/HotelImageServiceImpl.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @author user
  */
 @Service
-public class HotelImageServletImpl implements HotelImageService
+public class HotelImageServiceImpl implements HotelImageService
 {
     private HotelImageDAO hotelImageDAO ;
     
